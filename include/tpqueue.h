877ede6edbf9d25ab689cfe905f14595ca67e6a5
Maxim-Belyev <125187526+Maxim-Belyev@users.noreply.github.com>
2023-04-30 21:49:43 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,12 +1,35 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
-template<typename T>
+#include <list>
+template<class T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+ private:
+    std::list <T> LIST;
 
+ public:
+    T pop() {
+        T elem = LIST.front()
+        LIST.pop_front();
+        return elem;
+    }
+    void push(const T& value) {
+        if (LIST.empty()) {
+            LIST.push_back(value);
+        } else {
+            bool f = true;
+            for (auto iter = LIST.begin(); iter != LIST.end(); iter++) {
+                if (value.prior > (*iter).prior) {
+                    LIST.insert(iter, value);
+                    f = false;
+                    break;
+            }
+            if (f) {
+                LIST.push_back(value);
+            }
+        }
+    }
+};
 struct SYM {
   char ch;
   int prior;
