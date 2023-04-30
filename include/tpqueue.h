1725b5dba3fa03fbf4f660538dbd5b017b3e7e18
Maxim-Belyev <125187526+Maxim-Belyev@users.noreply.github.com>
2023-04-30 22:00:26 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,34 +5,37 @@
 template<class T>
 class TPQueue {
  private:
-    std::list <T> LIST;
+    std::list <T> TPQueue;
 
  public:
     T pop() {
-        T elem = LIST.front()
-        LIST.pop_front();
-        return elem;
+        T el = LIST.front();
+        TPQueue.pop_front();
+        return el;
     }
     void push(const T& value) {
-        if (LIST.empty()) {
-            LIST.push_back(value);
+        if (TPQueue.empty()) {
+            TPQueue.push_back(value);
         } else {
-            bool f = true;
-            for (auto iter = LIST.begin(); iter != LIST.end(); iter++) {
-                if (value.prior > (*iter).prior) {
-                    LIST.insert(iter, value);
-                    f = false;
+            bool x = true;
+            for (auto i = TPQueue.begin(); i != TPQueue.end(); i++) {
+                if (value.prior > (*i).prior) {
+                    TPQueue.insert(i, value);
+                    x = false;
                     break;
             }
-            if (f) {
-                LIST.push_back(value);
+         }
+            if (x) {
+                TPQueue.push_back(value);
             }
         }
     }
 };
+
 struct SYM {
   char ch;
   int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+#endif  // INCLUDE_TPQUEUEH_
+
